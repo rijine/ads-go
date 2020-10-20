a60f40b566e7d9d96671c80200c6cdfa6070eccf
alengcm <alengcm@gmail.com>
2020-10-20 15:27:05 +1030

    feat(counters): Create new counter document if it doesn't already exist

--- pkg/counters/repository.go
+++ pkg/counters/repository.go
@@ -2,6 +2,7 @@ package counters
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/rijine/ads-api/internal/database"
@@ -26,28 +27,36 @@ func (r *repository) GetAndUpdate(countOf string) (int64, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	filter := bson.M{"countOf": countOf}
+	filter := bson.M{"countof": countOf}
 	var result Counter
 	err := database.Collection(COLLECTION).FindOne(ctx, filter).Decode(&result)
-	if err.Error() == "mongo: no documents in result" {
 
-		insrt := Counter{
-			CountOf: "user",
-			Count:   1,
-		}
-
-		_, err = database.Collection(COLLECTION).InsertOne(ctx, insrt)
-
-	}
 	if err != nil {
+		if err.Error() == "mongo: no documents in result" {
 
+			insrt := Counter{
+				CountOf: countOf,
+				Count:   1,
+			}
+
+			_, err = database.Collection(COLLECTION).InsertOne(ctx, insrt)
+			if err != nil {
+				fmt.Println("Counter error:", err)
+				return 0, err
+			}
+			return 1, nil
+		}
 		return 0, err
 	}
-	update := bson.D{{"$set", bson.M{"count": result.Count + 1}}}
+
+	ct1 := result.Count + 1
+
+	update := bson.M{"$set": bson.M{"count": ct1}}
 
 	_, err = database.Collection(COLLECTION).UpdateOne(ctx, filter, update)
 
 	if err != nil {
+		fmt.Println("update", err)
 		return 0, err
 	}
 
